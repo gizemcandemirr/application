e0f45204824c09006e24740e76a43a30d74ee30e
Gizem Candemir <gizemfener2727@gmail.com>
2022-11-22 18:50:50 +0300

    Navigation create

--- App.js
+++ App.js
@@ -1,12 +1,49 @@
-import { Text, View } from "react-native";
 import { TailwindProvider } from "tailwindcss-react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+
+function HomeScreen({ navigation }) {
+  return (
+    <View className="flex items-center justify-center">
+      <Button
+        onPress={() => navigation.navigate("Notifications")}
+        title="Go to notifications"
+      />
+    </View>
+  );
+}
+
+// export default function App() {
+//   return (
+//     <TailwindProvider>
+//       <NavigationContainer>
+//         <Drawer.Navigator initialRouteName="Home">
+//           <Drawer.Screen name="Home" component={HomeScreen} />
+//           <Drawer.Screen name="View" component={ViewScreen} />
+//         </Drawer.Navigator>
+//       </NavigationContainer>
+//     </TailwindProvider>
+//   );
+// }
+function NotificationsScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <TailwindProvider>
-      <View className="flex-1 items-center justify-center bg-white">
-        <Text className="text-red-400">Hello world!</Text>
-      </View>
+      <NavigationContainer>
+        <Drawer.Navigator initialRouteName="Home">
+          <Drawer.Screen name="Home" component={HomeScreen} />
+          <Drawer.Screen name="Notifications" component={NotificationsScreen} />
+        </Drawer.Navigator>
+      </NavigationContainer>
     </TailwindProvider>
   );
 }
---
+++ src/screens/ViewScreen.js
@@ -0,0 +1,7 @@
+import { Text, View } from "react-native";
+
+const ViewScreen = () => {
+  return <Text>Hello View</Text>;
+};
+
+export default ViewScreen;
